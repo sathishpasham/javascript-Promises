bf6e708b17d660e4af4edd869a0facf7fa8c4fd1
Pasham Sathish Kumar <sathishkumarpasham1998@gmail.com>
2024-04-15 19:06:11 +0530

    second commit

--- call.js
+++ call.js
@@ -1,13 +1,24 @@
-let person1={
-    name: "sathish kumar pasham",
-    age:26,
-    getName: function(){
-        console.log(`I am ${this.name} and I am ${this.age} old!!`)
-    }
-}
+// let person1={
+//     name: "sathish kumar pasham",
+//     age:26,
+//     getName: function(){
+//         console.log(`I am ${this.name} and I am ${this.age} old!!`)
+//     }
+// }
 
-let person2={
-    name:"pasham",
-    age:25
+// let person2={
+//     name:"pasham",
+//     age:25
+// }
+// person1.getName.call(person2)
+
+//other way of writing the call method
+
+let person3 = {
+    name:"sathish kumar",
+    age:26
 }
-person1.getName.call(person2)
+let getName=function(){
+    console.log(`I am ${this.name} and I am ${this.age} old!!`)
+}
+getName.call(person3);
